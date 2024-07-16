130fcca092ca21ba67d79d06f1fcb68b06268e6c
inesgcalvo <inglez@gmail.com>
2024-07-16 18:06:22 +0200

    feature: add MLFLow Tracking

--- customer_review_classifier/customer_review_classifier.py
+++ customer_review_classifier/customer_review_classifier.py
@@ -1,11 +1,120 @@
+# Constants & Hyperparameters to define
+VERBOSE = True
+RANDOM_SEED = 42
+EPOCHS = 10
+BATCH_SIZE = 32
+NUM_WORDS = 5000
+MAX_SEQ_LEN = 50
+EMBEDDING_DIM = 50
+NUM_FILTERS = 64
+KERNEL_SIZE = 5
+NUM_CLASSES = 2
+SAMPLE_FRAC = 0.000001
+EXPERIMENT_NAME = 'exp.1'
+MLFLOW_TRACKING_URI = 'http://localhost:5000/'
+
+
+# Ignore Warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# Import Libraries
+import pandas as pd
+from tqdm.notebook import tqdm
+
 import mlflow
 import mlflow.keras
+from mlflow import MlflowClient
+from mlflow.models import infer_signature
 
+
+from sklearn.metrics import classification_report
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, Conv1D, MaxPooling1D, Flatten, Dense
+
+# Disable XLA JIT Compilation
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+# Import Functions
+import sys
+sys.path.append('../src')
+from preprocessing import DataInspection, HandleMissingValues, RemoveDuplicates, TranslateText, HandleOutliers, SplitDataset
+
+# Initialize Preprocessing Steps
+data_inspection = DataInspection()
+handle_missing_values = HandleMissingValues()
+remove_duplicates = RemoveDuplicates()
+translate_text = TranslateText()
+handle_outliers = HandleOutliers()
+split_dataset = SplitDataset()
+
+# Chain Preprocessing Steps
+data_inspection.set_next(handle_missing_values)
+handle_missing_values.set_next(remove_duplicates)
+remove_duplicates.set_next(translate_text)
+translate_text.set_next(handle_outliers)
+handle_outliers.set_next(split_dataset)
+
+# Load Data
+raw_data = pd.read_csv('../data/raw_data.csv')
+
+# Sample the data
+raw_data = raw_data.sample(frac=SAMPLE_FRAC, random_state=RANDOM_SEED, ignore_index=True)
+
+# Execute the pipeline
+X_train, X_test, y_train, y_test = data_inspection.process(raw_data, VERBOSE)
+
+padded_sequences = tf.convert_to_tensor(X_train, dtype=tf.float32)
+labels = tf.convert_to_tensor(y_train, dtype=tf.float32)
+
+# MLflow Logging and Model Training
 with mlflow.start_run():
-    mlflow.log_param('epochs', 10)
-    mlflow.log_param('batch_size', 32)
 
-    history = best_model.fit(X_padded, y, epochs=10, validation_split=0.2)
-    mlflow.log_metric('accuracy', max(history.history['val_accuracy']))
-    
-    mlflow.keras.log_model(best_model, "model")
+    client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment(EXPERIMENT_NAME)
+
+    mlflow.log_param('RANDOM_SEED', RANDOM_SEED)
+    mlflow.log_param('EPOCHS', EPOCHS)
+    mlflow.log_param('BATCH_SIZE', BATCH_SIZE)
+    mlflow.log_param('NUM_WORDS', NUM_WORDS)
+    mlflow.log_param('MAX_SEQ_LEN', MAX_SEQ_LEN)
+    mlflow.log_param('EMBEDDING_DIM', EMBEDDING_DIM)
+    mlflow.log_param('NUM_FILTERS', NUM_FILTERS)
+    mlflow.log_param('KERNEL_SIZE', KERNEL_SIZE)
+    mlflow.log_param('NUM_CLASSES', NUM_CLASSES)
+    mlflow.log_param('SAMPLE_FRAC', SAMPLE_FRAC)
+
+    # Define the CNN model
+    cnn_model = Sequential([
+        Embedding(input_dim=NUM_WORDS, output_dim=EMBEDDING_DIM, input_length=MAX_SEQ_LEN),
+        Conv1D(filters=NUM_FILTERS, kernel_size=KERNEL_SIZE, activation='relu', padding='same'),
+        MaxPooling1D(pool_size=4, padding='same'),
+        Flatten(),
+        Dense(10, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+
+    # Compile the model
+    cnn_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', 'precision', 'recall'])
+
+    # Fit the model    
+    history = cnn_model.fit(x=padded_sequences, y=labels, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_split=0.2)
+
+    # Metrics
+    y_pred = cnn_model.predict(X_test)
+    y_pred = (y_pred > 0.5).astype(int)
+    cr = classification_report(y_test, y_pred, output_dict=True)
+
+    mlflow.log_metric('accuracy', cr.pop('accuracy'))
+    for class_or_avg, metrics_dict in cr.items():
+        for metric, value in metrics_dict.items():
+            mlflow.log_metric(class_or_avg + '_' + metric, value)
+
+    # Model Signature
+    signature = infer_signature(X_train, cnn_model.predict(X_train))
+
+    mlflow.keras.log_model(cnn_model, 'customer review classifier', signature=signature, registered_model_name='cnn_classifier')
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,23 +1,8 @@
-# Constants & Hyperparameters to define
-RANDOM_SEED = 42
-NUM_WORDS = 5000
-MAX_SEQ_LEN = 50
-EMBEDDING_DIM = 50
-NUM_FILTERS = 64
-KERNEL_SIZE = 5
-NUM_CLASSES = 2
-
-######################################################################
-######################################################################
-######################################################################
-
 # Import Libraries
 import os
-import csv
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-import tensorflow as tf
 
 # Options
 pd.options.display.float_format = '{:.2f}'.format
@@ -25,10 +10,6 @@ pd.options.display.float_format = '{:.2f}'.format
 # Ignore Warnings
 import warnings
 warnings.filterwarnings('ignore')
-
-######################################################################
-######################################################################
-######################################################################
 
 class Preprocessing:
 
@@ -38,17 +19,13 @@ class Preprocessing:
     def set_next(self, step):
         self.next_step = step
     
-    def process(self, data):
+    def process(self, data, VERBOSE):
         if self.next_step:
-            return self.next_step.process(data)
+            return self.next_step.process(data, VERBOSE)
         return data
     
-
-
     @staticmethod
     def detect_language(text):
-        '''
-        '''
         from langdetect import detect
 
         try:
@@ -57,22 +34,16 @@ class Preprocessing:
         except:
             return 'unknown'
         
-
-
+    @staticmethod
     def translate_text(text, translator):
-        '''
-        '''
         try:
             translation = translator.translate(text, src='es', dest='en')
             return translation.text
         except:
             return text
         
-
-
-    def text_preprocessing(data):
-        '''
-        '''
+    @staticmethod
+    def text_preprocessing(data, NUM_WORDS, MAX_SEQ_LEN):
         from tensorflow.keras.preprocessing.text import Tokenizer
         from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -80,94 +51,87 @@ class Preprocessing:
         tokenizer.fit_on_texts(data['text'])
         sequences = tokenizer.texts_to_sequences(data['text'])
         padded_sequences = pad_sequences(sequences, maxlen=MAX_SEQ_LEN)
-        # padded_sequences = return tf.convert_to_tensor(padded_sequences, dtype=tf.float32)
         padded_sequences = np.array(padded_sequences, dtype=np.float32)
         return padded_sequences
 
-
-
-
-    def encode_labels(data):
-        '''
-        '''
+    @staticmethod
+    def encode_labels(data, VERBOSE):
         labels = pd.get_dummies(data['labels'], dtype=int)
-        # labels = tf.convert_to_tensor(labels['__label__1'], dtype=tf.float32)
         labels = np.array(labels['__label__1'], dtype=np.float32)
-        print(f'\nLABELS:\n{labels[0]}\n')
+        if VERBOSE: print(f'\nLABELS:\n{labels[0]}\n') 
         return labels
-
-######################################################################
-######################################################################
-######################################################################
 
 class DataInspection(Preprocessing):
 
-    def process(self, data):
-        '''
-        View Data Structure
-        Info Summary
-        Summary Statistics
-        '''
-        print('\n**View Data Structure**')
-        print('\nHEAD')
-        display(data.head())
-        print('\nSAMPLE')
-        display(data.sample(5))
-        print('\nTAIL')
-        display(data.tail())
-        print('\n**Info Summary**\n')
-        data.info(memory_usage='deep')
-        print('\n**Summary Statistics**')
-        display(data.describe())
+    def process(self, data, VERBOSE):
+        if VERBOSE: 
+            print('\n**View Data Structure**')
+            print('\nHEAD')
+            try:
+                display(data.head())
+            except:
+                print(data.head())
+            print('\nSAMPLE')
+            try:
+                display(data.sample(5))
+            except:
+                print(data.sample(5))
+            print('\nTAIL')
+            try:
+                display(data.tail())
+            except:
+                print(data.tail())
+            print('\n**Info Summary**\n')
+            data.info(memory_usage='deep')
+            print('\n**Summary Statistics**')
+            try:
+                display(data.describe())
+            except:
+                print(data.describe())
 
-        return super().process(data)
+        return super().process(data, VERBOSE)
 
-######################################################################
-   
 class HandleMissingValues(Preprocessing):
 
-    def process(self, data):
-        '''
-        '''
-        print('\n**Handle Missing Values**\n')
-        print(data.isna().sum().sort_values(ascending = False))
+    def process(self, data, VERBOSE):
+        if VERBOSE: 
+            print('\n**Handle Missing Values**\n')
+            print(data.isna().sum().sort_values(ascending = False))
 
         for column in data.columns:
             data[column].fillna('no title', inplace=True)
 
-        print('\nActual Missing Values\n')
-        print(data.isna().sum().sort_values(ascending = False))
+        if VERBOSE: 
+            print('\nActual Missing Values\n')
+            print(data.isna().sum().sort_values(ascending = False))
 
-        return super().process(data)
-
-######################################################################
+        return super().process(data, VERBOSE)
 
 class RemoveDuplicates(Preprocessing):
 
-    def process(self, data):
-        '''
-        '''
-        print('\n**Remove Duplicates**')
+    def process(self, data, VERBOSE):
+        if VERBOSE: 
+            print('\n**Remove Duplicates**')
         
         if data.duplicated().any():
-            print('\nYes, there are duplicates.')
-            print(f'SHAPE BEFORE: {data.shape}')
+            if VERBOSE:
+                print('\nYes, there are duplicates.')
+                print(f'SHAPE BEFORE: {data.shape}')
             data.drop_duplicates(inplace=True)
         else:
-            print('\nThere are no duplicated values.')
+            if VERBOSE:
+                print('\nThere are no duplicated values.')
 
-        print(f'ACTUAL SHAPE: {data.shape}')
+        if VERBOSE:
+            print(f'ACTUAL SHAPE: {data.shape}')
 
-        return super().process(data)
-
-######################################################################
+        return super().process(data, VERBOSE)
 
 class TranslateText(Preprocessing):
 
-    def process(self, data):
-        '''
-        '''
-        print('\n**Translate Text**')
+    def process(self, data, VERBOSE):
+        if VERBOSE:
+            print('\n**Translate Text**')
 
         from googletrans import Translator
         translator = Translator()
@@ -185,11 +149,9 @@ class TranslateText(Preprocessing):
                 english_translations.append(translation)
                 original_indices.append(idx)
                 english_texts.append(translation)
-                
             else:
                 english_texts.append(text)
 
-        # Create the DataFrame with the desired columns
         translations_df = pd.DataFrame({
             'spanish_texts': spanish_texts,
             'english_translations': english_translations,
@@ -198,106 +160,76 @@ class TranslateText(Preprocessing):
 
         data['text'] = english_texts
 
-        # Save the processed DataFrame
         translations_df.to_csv('../data/translations.csv', index=False)
         data.to_csv('../data/data_preprocessed.csv', index=False)
 
-        return super().process(data)
-    
-
-######################################################################
+        return super().process(data, VERBOSE)
 
 class HandleOutliers(Preprocessing):
 
-    def process(self, data):
-        '''
-        '''
-        print('\n**Handle Outliers**')
+    def process(self, data, VERBOSE):
+        if VERBOSE:
+            print('\n**Handle Outliers**')
 
         data['text_words'] = data['text'].apply(lambda x: len(x.split()))
 
-        # Calculate IQR
         Q1 = np.percentile(data['text_words'], 25)
         Q3 = np.percentile(data['text_words'], 75)
         IQR = Q3 - Q1
 
-
         IQR_min_text_words = Q1 - 1.5 * IQR
         IQR_max_text_words = Q3 + 1.5 * IQR
 
-        # Detect outliers using IQR method
         detected_outliers = data['text_words'][(data['text_words'] < IQR_min_text_words) | (data['text_words'] > IQR_max_text_words)]
 
-        print(f'\nNumber of detected outliers: {len(detected_outliers)}')
+        if VERBOSE:
+            print(f'\nNumber of detected outliers: {len(detected_outliers)}')
+            print(f'SHAPE BEFORE: {data.shape}')
 
-        print(f'SHAPE BEFORE: {data.shape}')
         data = data[data['text_words'] >= IQR_min_text_words]
         data = data[data['text_words'] <= IQR_max_text_words]
-        print(f'ACTUAL SHAPE: {data.shape}\n')
+
+        if VERBOSE:
+            print(f'ACTUAL SHAPE: {data.shape}\n')
 
         data.reset_index(inplace=True)
         data.to_csv('../data/data_preprocessed.csv', index=False)
 
-        return super().process(data)
-
-######################################################################
+        return super().process(data, VERBOSE)
 
 class SplitDataset(Preprocessing):
 
-    def process(self, data):
-        '''
-        '''
+    def process(self, data, VERBOSE):
         from sklearn.model_selection import train_test_split
 
-        padded_sequences = Preprocessing.text_preprocessing(data)
-        labels = Preprocessing.encode_labels(data)
+        padded_sequences = Preprocessing.text_preprocessing(data, NUM_WORDS=5000, MAX_SEQ_LEN=50)
+        labels = Preprocessing.encode_labels(data, VERBOSE)
 
         X_train, X_test, y_train, y_test = train_test_split(
             padded_sequences, 
             labels, 
             test_size=0.2, 
-            random_state=RANDOM_SEED)
+            random_state=42)
         
-        print('X_train shape:', X_train.shape)
-        print('X_test shape:', X_test.shape)
-        print('y_train shape:', y_train.shape)
-        print('y_test shape:', y_test.shape)
+        if VERBOSE:
+            print('X_train shape:', X_train.shape)
+            print('X_test shape:', X_test.shape)
+            print('y_train shape:', y_train.shape)
+            print('y_test shape:', y_test.shape)
 
-        # Save the split datasets if needed
         self.save_to_csv(X_train, 'X_train.csv')
         self.save_to_csv(X_test, 'X_test.csv')
         self.save_to_csv(y_train, 'y_train.csv')
         self.save_to_csv(y_test, 'y_test.csv')
 
-        # Save data as CSV files
-        '''
-        data_dir = os.path.join('..', 'data')
-        os.makedirs(data_dir, exist_ok=True)
-
-        data_list = [(X_train, 'X_train.csv'), (X_test, 'X_test.csv'), (y_train, 'y_train.csv'), (y_test, 'y_test.csv')]
-        for data, filename in data_list:
-            try:
-                with open(os.path.join(data_dir, filename), 'w', newline='') as csvfile:
-                    csv_writer = csv.writer(csvfile)
-                    csv_writer.writerows(data.tolist() if isinstance(data[0], list) else data)
-            except IOError as e:
-                print(f'Error saving data to {filename}: {e}')
-
-        print('\nData saved successfully to ../data directory in CSV format.')
-        ''';
-
         return X_train, X_test, y_train, y_test
     
-
-
-    def save_to_csv(self, data, filename):
-        '''
-        '''
+    @staticmethod
+    def save_to_csv(data, filename):
         data_dir = '../data'
         os.makedirs(data_dir, exist_ok=True)
         filepath = os.path.join(data_dir, filename)
 
-        # If data is 1-dimensional, reshape it to 2D for CSV
         if data.ndim == 1:  
             data = data.reshape(-1, 1)
 
